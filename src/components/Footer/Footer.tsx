389dd112de3fc558f5497a34a0f846da69755008
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2022-10-24 14:47:47 -0300

    a

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer () {
 
     return (
         <FooterContainer>
-            Este site foi construído com o propósito de treinamento em ReactJs. Programado por <a href="https://github.com/Hime-chan" target="_blank">Claudia Cavalcante Fonseca</a> e <a href="https://github.com/xnd-dev">Alexandre Barbosa</a>. A empresa Go Nuts Donuts é fictícia, assim como os produtos aqui apresentados.
+            Este site foi construído com o propósito de treinamento em ReactJs. Programado por <a href="https://github.com/Hime-chan" target="_blank" rel="noreferrer">Claudia Cavalcante Fonseca</a> e <a rel="noreferrer" href="https://github.com/xnd-dev">Alexandre Barbosa</a>. A empresa Go Nuts Donuts é fictícia, assim como os produtos aqui apresentados.
         </FooterContainer>
     )
 } 
