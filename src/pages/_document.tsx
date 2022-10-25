f626859682ac85457ebac67572b0606f56364aa4
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2022-10-25 18:47:28 -0300

    No fav icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,9 +20,8 @@ export default function Document() {
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin = "anonymous"/>
 				<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700;800&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
 				
-				<link rel="icon" href="/favicon1.png" />
 			</Head>
-			<body>
+			<body>	
 				<Main />				
 				<NextScript />
 			</body>
