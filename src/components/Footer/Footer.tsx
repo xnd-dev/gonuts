c18a6d1bb0f633c98a17a54b7e4203e5fa1ebcf4
Alexandre Barbosa <xnd.dev@gmail.com>
2024-10-29 18:45:55 -0300

    deploy

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,6 @@ export function Footer () {
             <div className="center">
                 <p>Este site foi construído com o propósito de treinamento em ReactJs. Programado por <a href="https://www.linkedin.com/in/hihccf/" target="_blank" rel="noreferrer">Claudia Cavalcante Fonseca</a> e <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/alexandre-barbosa-775426239/">Alexandre Barbosa</a>. A empresa Go Nuts Donuts é fictícia, assim como os produtos aqui apresentados.</p>
                 <p>Todas as ilustrações utilizadas foram adquiridas no banco de imagens do <a href="https://www.pexels.com" rel="noreferrer" target="_blank">Pexels</a>.</p>
-
             </div>
         </FooterContainer>
     )
